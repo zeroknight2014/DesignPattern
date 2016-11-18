0b76752cf68eb805d5d576031219a568bb03add3
liuzhuan <liuzhuan@shuame.com>
2016-11-18 14:54:38 +0800

    单例模式

---
+++ singleton.h
@@ -0,0 +1,36 @@
+//
+// Created by zeroknight on 2016/11/18.
+//
+
+#ifndef DESIGNPATTERN_SINGLETON_H
+#define DESIGNPATTERN_SINGLETON_H
+
+//未完成：
+//将复制构造函数和=操作符都设为私有
+
+
+//饿汉式单例, 在类被加载时，就会实例化一个对象
+class HungrySingleton {
+private:
+    static HungrySingleton *instance;
+
+    HungrySingleton() {};
+public:
+    static HungrySingleton *getInstance();
+};
+
+//懒汉式单例, 在调用取得实例方法时，才会实例化一个对象
+class Singleton {
+private:
+    static Singleton *instance;
+
+    Singleton() {};
+public:
+    static Singleton *getInstance();
+
+    static Singleton *getInstance2();
+
+    static Singleton *getInstance3();
+};
+
+#endif //DESIGNPATTERN_SINGLETON_H
