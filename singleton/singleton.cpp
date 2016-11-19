f0a45153ea7025012571abd97e0ea9ef1c86a84c
liuzhuan <liuzhuan@shuame.com>
2016-11-19 20:56:00 +0800

    单例模式总结和实现

---
+++ singleton/singleton.cpp
@@ -0,0 +1,66 @@
+//
+// Created by zeroknight on 2016/11/17.
+//
+#include <iostream>
+#include "singleton.h"
+
+
+HungrySingleton *HungrySingleton::instance = new HungrySingleton();
+HungrySingleton *HungrySingleton::getInstance(){
+ return instance;
+}
+
+
+Singleton *Singleton::instance = NULL;
+
+//通用写法，但是多线程时，有问题
+Singleton *Singleton::getInstance() {
+    if (instance == NULL)
+        instance = new Singleton();
+    return instance;
+}
+
+//多线程时，性能不是很好，每次判断都会锁定，造成大量线程阻塞
+Singleton *Singleton::getInstance2() {
+//    lock();
+    if (instance == NULL){
+        instance = new Singleton();
+    }
+//    unlock();
+    return instance;
+}
+
+//增强版，多线程时，减少逻辑判断导致的线程阻塞
+Singleton *Singleton::getInstance3() {
+    if (instance == NULL){
+//        lock();
+        if (instance == NULL) {
+            instance = new Singleton();
+        }
+//        unlock();
+
+    }
+    return instance;
+}
+
+//释放唯一的对象
+void Singleton::destroyInstance() {
+    if (instance != NULL) {
+        delete instance;
+        instance = NULL;
+    }
+}
+
+int main() {
+    HungrySingleton *mHSingleton1 = HungrySingleton::getInstance();
+    HungrySingleton *mHSingleton2 = HungrySingleton::getInstance();
+    if (mHSingleton1 == mHSingleton2) {
+        std::cout << "HungrySingleton1 = HungrySingleton2!" << std::endl;
+    }
+    Singleton *mSingleton1 = Singleton::getInstance();
+    Singleton *mSingleton2 = Singleton::getInstance();
+    if (mSingleton1 == mSingleton2) {
+        std::cout << "singleton1 = singleton2!" << std::endl;
+    }
+    return 0;
+}
